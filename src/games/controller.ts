0538f9bbd9756ba299dc75878d5f84dd2c08bfe9
Scythe-11 <christophernmills@gmail.com>
2018-04-21 08:34:32 +0200

    added functionality for post and put - also set up dependancies for typeOrm and PG

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -1,5 +1,5 @@
 // src/pages/controller.ts
-import { JsonController, Get, Param } from 'routing-controllers'
+import { JsonController, Get, Put, Post, Param } from 'routing-controllers'
 import gamesById, { Game } from './data'
 
 @JsonController()
@@ -16,4 +16,20 @@ export default class GameController {
     allGames(): GameList {
         return {games: Object.values(gamesById)}
     }
+    @Put('/games/:id')
+    updateGame(
+        @Param('id') id: number,
+        @Body() body: Partial<Game>
+    ): Game {
+        console.log(`Incoming PUT body param:`, body)
+        return gamesById[id]
+    }
+    @Post('/games')
+    @HttpCode(201)
+    createGame(
+        @Body() body: Game
+    ): Game {
+        console.log(`Incoming POST body param:`, body)
+        return body
+}
 }
